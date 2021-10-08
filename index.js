a65ef50adf802c4d7fca980ace1b3b55af1248d1
Jorge Basauri Espinoza <jbasaurie15@unc.edu.pe>
2021-10-08 00:17:26 -0500

    Terminando autenticacion y acceso de servicios

--- index.js
+++ index.js
@@ -6,17 +6,15 @@ const cors = require('cors');
 const app=express();
 //estableciendo la configuración de cors
 app.use(cors());
+//Lectura y parseo del body
+app.use(express.json());
 //crear la conexión a la BD
 dbConection();
 //verificando variables de entorno
 console.log(process.env);
-// Creando 
-app.get('/',(req,res)=>{
-    res.json({
-        ok:true,
-        msg: 'Bienvenidos a node'
-    });
-})
+//Rutas de la API
+app.use('/api/usuarios', require('./routes/usuarios.routes'));
+app.use('/api/login', require('./routes/auth.routes'));
 //codigo para desplegar el servidor
 app.listen(process.env.PORT,()=>{
     console.log('Servidor Node.js desplegado en el puerto: '+process.env.PORT);
